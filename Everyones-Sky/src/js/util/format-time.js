d7add4b0312958ae1afea2bf6464b29b7c04e5c5
xXGmoneyz <122395097+xXGmoneyz@users.noreply.github.com>
2023-02-09 10:32:43 -0500

    Delete Everyones-Sky directory

--- Everyones-Sky/src/js/util/format-time.js
+++
@@ -1,9 +0,0 @@
-addZeroes = text => {
-    text = text + '';
-    while (text.length < 2) {
-        text = '0' + text;
-    }
-    return text;
-};
-
-formatTime = t => addZeroes(~~(t / 60)) + ':' + addZeroes(~~t % 60);
